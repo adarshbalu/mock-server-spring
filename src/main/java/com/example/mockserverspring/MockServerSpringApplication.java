a8c0b636c583fd581a5139b9f65582f2b21bbe09
Adarsh Balachandran <Adarsh.Balachandran@hatio.in>
2021-10-22 16:54:28 +0530

    Initial commit

---
+++ src/main/java/com/example/mockserverspring/MockServerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockserverspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockServerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockServerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mockserverspring/MockServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockserverspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockServerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
