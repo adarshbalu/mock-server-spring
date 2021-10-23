7640a51918efd7f2c58d6cc7c8115234b41030d7
Adarsh Balachandran <Adarsh.Balachandran@hatio.in>
2021-10-23 14:56:16 +0530

    Add getter and setter for class mock and request

--- src/main/java/com/example/mockserverspring/Request.java
+++ src/main/java/com/example/mockserverspring/Request.java
@@ -4,11 +4,88 @@ import java.util.HashMap;
 
 public class Request {
     private int id;
+
+    public Request(int id, int status, String method, String endPoint, String mockName, HashMap<String, Object> response, HashMap<String, Object> body, HashMap<String, Object> params) {
+        this.id = id;
+        this.status = status;
+        this.method = method;
+        this.endPoint = endPoint;
+        this.mockName = mockName;
+        this.response = response;
+        this.body = body;
+        this.params = params;
+    }
+
     private int status;
     private String method;
     private String endPoint;
     private String mockName;
     private HashMap<String,Object> response;
     private HashMap<String,Object> body;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(String endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public String getMockName() {
+        return mockName;
+    }
+
+    public void setMockName(String mockName) {
+        this.mockName = mockName;
+    }
+
+    public HashMap<String, Object> getResponse() {
+        return response;
+    }
+
+    public void setResponse(HashMap<String, Object> response) {
+        this.response = response;
+    }
+
+    public HashMap<String, Object> getBody() {
+        return body;
+    }
+
+    public void setBody(HashMap<String, Object> body) {
+        this.body = body;
+    }
+
+    public HashMap<String, Object> getParams() {
+        return params;
+    }
+
+    public void setParams(HashMap<String, Object> params) {
+        this.params = params;
+    }
+
     private HashMap<String,Object> params;
 }
